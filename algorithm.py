72d8f29dbd8d55870a45efc36fca2c802959388e
Cameron Stark <starkc920@outlook.com>
2020-04-22 23:33:37 -0400

    completed code

--- algorithm.py
+++ algorithm.py
@@ -1,34 +1,68 @@
 from time import sleep
 import numpy as np
+import random
 import pandas as pd
 
-from sklearn import linear_model
+import plotly.graph_objects as go
+import plotly.express as px
 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
-def performRegression(data):
+def performRegression(data, stock, forecast):
     
     #Sorting the data into proper date and time
     data['Date'] = pd.to_datetime(data.Date, format ='%Y-%m-%d')
     data.index = data['Date']
     #displaying it in descending order
     data = data.sort_index(ascending = True, axis=0)
-    #seperate dataset so that new features wont affect data
-    new_data = pd.DataFrame(index=range(0,len(data)),columns=['Date','Close'])
-    
-    for i in range(0,len(data)):
-        new_data['Date'][i] = data['Date'][i]
-        new_data['Close'][i] = data['Close'][i]
 
-    for i in range(0, len(new_data)):
-        if (new_data['Date'][i].weekday() == 0 or new_data['Date'][i].weekday()):
-            new_data['mon_fri'][i] = 1
-        else:
-            new_data['mon_fri'][i] = 0
+    data = data[['Close']]
+
+    forecast_out = forecast
+    data['Prediction'] = data[['Close']].shift(-forecast_out)
+
+    X = np.array(data.drop(['Prediction'], 1))
+    X = X[:-forecast_out]
+
+
+    y = np.array(data['Prediction'])
+    y = y[:-forecast_out]
+
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    model = LinearRegression()
+
+    model.fit(x_train, y_train)
+
+    forecast_prices = np.array(data.drop(['Prediction'], 1))[-forecast_out:]
+    predictions = model.predict(forecast_prices)
+
     
+    j = forecast_out
+    j_alt = forecast_out - 1
+    dataLength = len(data) - 1
+
+
+    for i in range(j):
+        data['Prediction'][dataLength - i] = predictions[j_alt - i]
+
+    xAxis = data.index.values
+    closeData = {'Date' : xAxis[:-forecast_out], 'Close' : y} 
+    closeValues = pd.DataFrame(data = closeData)
+    predictedData = {'Date' : xAxis[len(xAxis)-forecast_out:len(xAxis)], 'Predicted' : predictions}
+    predictedValues = pd.DataFrame(data = predictedData)
+
+    fig = px.line(closeValues, x='Date', y='Close')
+    fig.add_trace(go.Scatter(x=predictedValues['Date'], y=predictedValues['Predicted'], name="Predicted Values"))
+    title = str(forecast) + " Day Forecast For " + stock  
+    fig.update_layout(title=title, xaxis_title = "Dates", yaxis_title = "Cost Per Share ($)")
+    fig.show()
+    return
 
 def loadData(stock):
     file = "Stocks/" + stock + ".us.txt"
-    i = 0
+    i = 0 
     headers = []
     data = []
     with open(file, "r") as stockFile:
@@ -40,25 +74,20 @@ def loadData(stock):
                 data.append(line.strip().replace(" ", "").split(","))
 
     dataframe = pd.DataFrame(data = data, columns=headers)
-    return dataframe
 
-def algMain(listOfStocks):
+    split = np.array_split(dataframe, 2)
+
+    return pd.DataFrame(data = split[1])
+
+def algMain(listOfStocks, forecast):
 
     #performs regression for each stock individually
     for stock in listOfStocks:
         dataframe = loadData(stock)
-        print(dataframe)
-        #performRegression(dataframe)
-        dateTesting(dataframe)
+        #print(dataframe)
+        performRegression(dataframe, stock.upper(), int(forecast))
+    return
+        
 
 
-def dateTesting(data):
-    #Sorting the data into proper date and time
-    data['Date'] = pd.to_datetime(data.Date, format ='%Y-%m-%d')
-    data.index = data['Date']
-    #displaying it in descending order
-    data = data.sort_index(ascending = True, axis=0)
-
-    date = data['Date'][0]
-    print(date.weekday())
     
--- main.py
+++ main.py
@@ -4,24 +4,34 @@ from os import system, name
 from algorithm import algMain
 
 def printTitle():
-    print("\t----- Stock Market Prediction and Portfolio Tool -----")
+    print("\t----- Stock Market Prediction Tool -----")
 
 def getStocksFromUser():
     print("Enter The Stocks In Question, Separated By Commas")
     stocks = input(">>> ")
     return stocks
 
-def runAlgorithm(listOfStocks):
+def getForcastFromUser():
+    print("Enter How Many Days To Forecast Stock Prices")
+    forecast = input(">>> ")
+    return forecast
+
+@animation.wait('spinner')
+def runAlgorithm(listOfStocks, forecast):
     clear()
-    print("Running Predictive Pricing of", listOfStocks, "stocks")
-    algMain(listOfStocks)
+    print("Running Predictive Pricing of", listOfStocks, "Stocks At A", forecast, "Day Forecast")
+    loading = animation.Wait()
+    loading.start()
+    algMain(listOfStocks, forecast)
+    loading.stop()
 
 def main():
     clear()
     printTitle()
     stocksText = getStocksFromUser()
     stocks = stocksText.lower().replace(" ", "").split(",")
-    runAlgorithm(stocks)
+    forecast = getForcastFromUser()
+    runAlgorithm(stocks, forecast)
     #print(stocks)
     
 def clear():
